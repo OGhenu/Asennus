e59b2ed731a44ef51af4c52f5879f32224605ddd
OGhenu <henri.pernila@gmail.com>
2016-11-14 09:47:39 +0000

    better version

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,29 +1,31 @@
-class Asennus {
-package {"apache2":
-ensure => "installed",
-}
+class asennus {
 
-file {"/var/www/html/index.html":
-content => "Welcome.\n",
-}
+	package {"apache2":
+	ensure => "installed",
+	allowcdrom => "true",
+	}
 
-file { '/etc/apache2/mods-enabled/userdir.conf':
-ensure => 'link',
-target => '../mods-available/userdir.conf',
-notify => Service["apache2"],
-}
+	file {"/var/www/html/index.html":
+	content => "Welcome.\n",
+	}
 
-file { '/etc/apache2/mods-enabled/userdir.load':
-ensure => 'link',
-target => '../mods-available/userdir.load',
-notify => Service["apache2"],
-}
+	file { '/etc/apache2/mods-enabled/userdir.conf':
+	ensure => 'link',
+	target => '../mods-available/userdir.conf',
+	notify => Service["apache2"],
+	}
 
-service {"apache2":
-ensure => "true",
-enable => "true",
-# provider => "systemd",
-}
+	file { '/etc/apache2/mods-enabled/userdir.load':
+	ensure => 'link',
+	target => '../mods-available/userdir.load',
+	notify => Service["apache2"],
+	}
+
+	service {"apache2":
+	ensure => "true",
+	enable => "true",
+	# provider => "systemd",
+	}
 
 }
 
